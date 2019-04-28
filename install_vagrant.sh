780398ec767ad8b1e2fb2a2385afe86261db7d49
androidseb <theandroidseb@gmail.com>
2019-04-28 01:13:11 -0400

    added missing step to the main install process: installing virtualbox

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -3,6 +3,9 @@
 # Exit on any error encountered
 set -e
 
+# Install virtualbox
+apt-get install -y virtualbox
+
 # Install vagrant
 wget https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
 dpkg -i vagrant_2.2.4_x86_64.deb
