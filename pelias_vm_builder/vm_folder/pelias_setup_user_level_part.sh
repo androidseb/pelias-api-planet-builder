2984d98bd031190b17ff9744ffe051392dcd9379
androidseb <theandroidseb@gmail.com>
2019-05-05 15:14:03 -0400

    separated grouped steps, to allow more granular errors visibility

--- pelias_vm_builder/vm_folder/pelias_setup_user_level_part.sh
+++ pelias_vm_builder/vm_folder/pelias_setup_user_level_part.sh
@@ -3,7 +3,15 @@
 # Exit on any error encountered
 set -e
 
-echo "$(date) - User level setup - start">>~/logs_pelias_setup.txt
+MAIN_LOG_FILE_PATH=~/logs_pelias_setup.txt
+
+
+################################################################################
+# Step 1: Download and configure base files
+################################################################################
+
+echo "$(date) - Step 1: Download and configure base files">>$MAIN_LOG_FILE_PATH
+
 cd $USERHOME
 
 # create directories
@@ -41,34 +49,98 @@ echo "COMPOSE_PROJECT_NAME=$USERNAME" > .env
 echo "DOCKER_USER=$(id -u $USERNAME)" >> .env
 echo "DATA_DIR=$USERHOME/data" >> .env
 
-# Keep running the script even when facing errors, some of the following
-# commands can, return non 0 exit status even though they are fine
-# For example
-# "pelias prepare all"
-# can error out with
-# "tiger download dir does not exist"
-# even the overall setup is fine
+
+# Keep running the script even when facing errors, leaving traces of
+# the return codes in the logs with the $? variable
 set +e
 
+
+################################################################################
+# Step 2: pelias compose pull
+################################################################################
+
 # update all docker images
-echo "$(date) - User level setup - compose pull">>~/logs_pelias_setup.txt
+echo "$(date) - Step 2: pelias compose pull">>$MAIN_LOG_FILE_PATH
 pelias compose pull 2>&1 | tee ~/logs_pelias_setup_setup_details_for_pull.txt
+echo "$(date) - Step 2: pelias compose pull => $?">>$MAIN_LOG_FILE_PATH
+
+
+################################################################################
+# Step 3: elastic search init
+################################################################################
 
 # start elasticsearch server
-echo "$(date) - User level setup - elastic start">>~/logs_pelias_setup.txt
+echo "$(date) - Step 3: elastic search init - start">>$MAIN_LOG_FILE_PATH
 pelias elastic start 2>&1 | tee ~/logs_pelias_setup_setup_details_for_es_start.txt
+echo "$(date) - Step 3: elastic search init - start => $?">>$MAIN_LOG_FILE_PATH
 
 # wait for elasticsearch to start up
-echo "$(date) - User level setup - elastic wait">>~/logs_pelias_setup.txt
+echo "$(date) - Step 3: elastic search init - wait">>$MAIN_LOG_FILE_PATH
 pelias elastic wait 2>&1 | tee ~/logs_pelias_setup_setup_details_for_es_wait.txt
+echo "$(date) - Step 3: elastic search init - wait => $?">>$MAIN_LOG_FILE_PATH
 
 # create elasticsearch index with pelias mapping
-echo "$(date) - User level setup - elastic create">>~/logs_pelias_setup.txt
+echo "$(date) - Step 3: elastic search init - create">>$MAIN_LOG_FILE_PATH
 pelias elastic create 2>&1 | tee ~/logs_pelias_setup_setup_details_for_es_create.txt
+echo "$(date) - Step 3: elastic search init - create => $?">>$MAIN_LOG_FILE_PATH
 
-# (re)download all data
-echo "$(date) - User level setup - download all">>~/logs_pelias_setup.txt
-pelias download all 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download.txt
+
+################################################################################
+# Step 4: pelias download
+################################################################################
+
+#(re)download whosonfirst data
+echo "$(date) - Step 4: pelias download - wof">>$MAIN_LOG_FILE_PATH
+pelias download wof 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download_wof.txt
+echo "$(date) - Step 4: pelias download - wof => $?">>$MAIN_LOG_FILE_PATH
+
+#(re)download openaddresses data
+echo "$(date) - Step 4: pelias download - oa">>$MAIN_LOG_FILE_PATH
+pelias download oa 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download_oa.txt
+echo "$(date) - Step 4: pelias download - oa => $?">>$MAIN_LOG_FILE_PATH
+
+#(re)download openstreetmap data
+echo "$(date) - Step 4: pelias download - osm">>$MAIN_LOG_FILE_PATH
+pelias download osm 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download_osm.txt
+echo "$(date) - Step 4: pelias download - osm => $?">>$MAIN_LOG_FILE_PATH
+
+#(re)download TIGER data
+echo "$(date) - Step 4: pelias download - tiger">>$MAIN_LOG_FILE_PATH
+pelias download tiger 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download_tiger.txt
+echo "$(date) - Step 4: pelias download - tiger => $?">>$MAIN_LOG_FILE_PATH
+
+#(re)download transit data
+echo "$(date) - Step 4: pelias download - transit">>$MAIN_LOG_FILE_PATH
+pelias download transit 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download_transit.txt
+echo "$(date) - Step 4: pelias download - transit => $?">>$MAIN_LOG_FILE_PATH
+
+#(re)download csv data
+echo "$(date) - Step 4: pelias download - csv">>$MAIN_LOG_FILE_PATH
+pelias download csv 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download_csv.txt
+echo "$(date) - Step 4: pelias download - csv => $?">>$MAIN_LOG_FILE_PATH
+
+
+################################################################################
+# Step 5: pelias prepare
+################################################################################
+
+# build all services which have a prepare step
+
+# Note: "pelias prepare polylines" is skipped because too heavy on memory for
+# a planet build and we downloaded the result as extract.0sv in step 1
+
+echo "$(date) - Step 5: pelias prepare interpolation">>$MAIN_LOG_FILE_PATH
+pelias prepare interpolation 2>&1 | tee ~/logs_pelias_setup_setup_details_for_prepare_interpolation.txt
+echo "$(date) - Step 5: pelias prepare interpolation => $?">>$MAIN_LOG_FILE_PATH
+
+echo "$(date) - Step 5: pelias prepare placeholder">>$MAIN_LOG_FILE_PATH
+pelias prepare placeholder 2>&1 | tee ~/logs_pelias_setup_setup_details_for_prepare_placeholder.txt
+echo "$(date) - Step 5: pelias prepare placeholder => $?">>$MAIN_LOG_FILE_PATH
+
+
+################################################################################
+# Step 6: pelias import
+################################################################################
 
 # Hack to prevent that type of error from happening (from my experience of March 25, 2019):
 # --------------------------------------------------------------------------------
@@ -80,20 +152,50 @@ pelias download all 2>&1 | tee ~/logs_pelias_setup_setup_details_for_download.tx
 # See https://github.com/pelias/dbclient/issues/76
 curl -XPUT localhost:9200/_cluster/settings -d '{ "transient" : { "threadpool.bulk.queue_size" : 500 } }'
 
-# build all services which have a prepare step
-# Note: "pelias prepare polylines" is skipped because too heavy on memory for
-# a planet build, and extract.0sv is already downloaded.
-echo "$(date) - User level setup - prepare interpolation">>~/logs_pelias_setup.txt
-pelias prepare interpolation 2>&1 | tee ~/logs_pelias_setup_setup_details_for_prepare_interpolation.txt
-echo "$(date) - User level setup - prepare placeholder">>~/logs_pelias_setup.txt
-pelias prepare placeholder 2>&1 | tee ~/logs_pelias_setup_setup_details_for_prepare_placeholder.txt
+# (re)import whosonfirst data
+echo "$(date) - Step 6: pelias import wof">>$MAIN_LOG_FILE_PATH
+pelias import wof 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import_wof.txt
+echo "$(date) - Step 6: pelias import wof => $?">>$MAIN_LOG_FILE_PATH
 
-# (re)import all data
-echo "$(date) - User level setup - import all">>~/logs_pelias_setup.txt
-pelias import all 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import.txt
+# (re)import openaddresses data
+echo "$(date) - Step 6: pelias import oa">>$MAIN_LOG_FILE_PATH
+pelias import oa 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import_oa.txt
+echo "$(date) - Step 6: pelias import oa => $?">>$MAIN_LOG_FILE_PATH
+
+# (re)import openstreetmap data
+echo "$(date) - Step 6: pelias import osm">>$MAIN_LOG_FILE_PATH
+pelias import osm 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import_osm.txt
+echo "$(date) - Step 6: pelias import osm => $?">>$MAIN_LOG_FILE_PATH
+
+# (re)import polylines data
+echo "$(date) - Step 6: pelias import polylines">>$MAIN_LOG_FILE_PATH
+pelias import polylines 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import_polylines.txt
+echo "$(date) - Step 6: pelias import polylines => $?">>$MAIN_LOG_FILE_PATH
+
+# (re)import geonames data
+echo "$(date) - Step 6: pelias import geonames">>$MAIN_LOG_FILE_PATH
+pelias import geonames 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import_geonames.txt
+echo "$(date) - Step 6: pelias import geonames => $?">>$MAIN_LOG_FILE_PATH
+
+# (re)import transit data
+echo "$(date) - Step 6: pelias import transit">>$MAIN_LOG_FILE_PATH
+pelias import transit 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import_transit.txt
+echo "$(date) - Step 6: pelias import transit => $?">>$MAIN_LOG_FILE_PATH
+
+# (re)import csv data
+echo "$(date) - Step 6: pelias import csv">>$MAIN_LOG_FILE_PATH
+pelias import csv 2>&1 | tee ~/logs_pelias_setup_setup_details_for_import_csv.txt
+echo "$(date) - Step 6: pelias import csv => $?">>$MAIN_LOG_FILE_PATH
+
+
+################################################################################
+# Step 7: pelias compose up
+################################################################################
 
 # start one or more docker-compose service(s)
-echo "$(date) - User level setup - compose up">>~/logs_pelias_setup.txt
+echo "$(date) - Step 7: compose up">>$MAIN_LOG_FILE_PATH
 pelias compose up 2>&1 | tee ~/logs_pelias_setup_setup_details_for_compose_up.txt
+echo "$(date) - Step 7: compose up => $?">>$MAIN_LOG_FILE_PATH
+
 
 echo "$(date) - User level setup - Setup completed with success!!!">>~/logs_pelias_setup.txt
